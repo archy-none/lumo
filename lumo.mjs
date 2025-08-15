51d732d93f941a756e4e3a7ca1f33351e8f82267
秋井乃音 <archy1none@gmail.com>
2025-08-15 17:08:58 +0900

    Create lumo.mjs

---
+++ lumo.mjs
@@ -0,0 +1,42 @@
+import { readFileSync } from "node:fs";
+import { createInterface } from "readline";
+import { lumo } from "./docs/runtime/node.mjs";
+
+const args = process.argv.slice(2);
+if (args.length === 0) {
+    let code = "";
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "> ",
+    });
+
+    console.log("Lumo REPL");
+    rl.prompt();
+
+    rl.on("line", (input) => {
+        if (input.trim() !== "")
+            lumo(`${code};${input}`)
+                .then((result) => {
+                    code += `;${input}`;
+                    if (result !== undefined) console.log(result);
+                })
+                .catch((e) => console.log("\u0007Error!", e))
+                .then(() => rl.prompt());
+        else rl.prompt();
+    });
+
+    rl.on("close", () => {
+        console.log("Bye");
+        process.exit(0);
+    });
+} else {
+    const filePath = args[0];
+    const source = readFileSync(filePath, "utf8");
+    try {
+        const result = await lumo(source.toString());
+        if (result !== undefined) console.log(result);
+    } catch (e) {
+        console.log("Error!", e);
+    }
+}
