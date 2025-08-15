f8d5ca4061984571f7f01eca28792d4809d41e5f
秋井乃音 <archy1none@gmail.com>
2025-08-15 17:07:39 +0900

    Update run.mjs

--- run.mjs
+++ run.mjs
@@ -3,15 +3,39 @@ import { lumo } from "./docs/runtime/node.mjs";
 
 const args = process.argv.slice(2);
 if (args.length === 0) {
-    console.log("Lumo Runtime 2025 Node.js Edition");
-    process.exit(1);
-}
+    let code = "";
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "> ",
+    });
 
-const filePath = args[0];
-const source = readFileSync(filePath, "utf8");
-try {
-    const result = await lumo(source.toString());
-    if (result !== undefined) console.log(result);
-} catch (e) {
-    console.log("Error!", e);
+    console.log("Lumo REPL");
+    rl.prompt();
+
+    rl.on("line", (input) => {
+        if (input.trim() !== "")
+            lumo(`${code};${input}`)
+                .then((result) => {
+                    code += `;${input}`;
+                    if (result !== undefined) console.log(result);
+                })
+                .catch((e) => console.log("\u0007Error!", e))
+                .then(() => rl.prompt());
+        else rl.prompt();
+    });
+
+    rl.on("close", () => {
+        console.log("Bye");
+        process.exit(0);
+    });
+} else {
+    const filePath = args[0];
+    const source = readFileSync(filePath, "utf8");
+    try {
+        const result = await lumo(source.toString());
+        if (result !== undefined) console.log(result);
+    } catch (e) {
+        console.log("Error!", e);
+    }
 }
