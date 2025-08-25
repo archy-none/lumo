ca20cc030c493d3a5e3e9cb395b2b02dbb63d2ee
秋井乃音 <archy1none@gmail.com>
2025-08-25 13:25:14 +0900

    Update utils.rs

--- core/src/utils.rs
+++ core/src/utils.rs
@@ -197,19 +197,6 @@ macro_rules! import_args {
 }
 
 #[macro_export]
-macro_rules! safeguard {
-    ($expr: expr, $typ: expr) => {{
-        let is_null = Expr::Operator(Box::new(
-            (Op::LNot(Expr::Operator(Box::new(Op::NullCheck(
-                *$expr.clone(),
-                true,
-            ))))),
-        ));
-        Stmt::If(is_null, Expr::Block(Block(vec![Stmt::Error])), None)
-    }};
-}
-
-#[macro_export]
 macro_rules! correct {
     ($lhs: expr, $rhs: expr , $ctx: expr, $pat: pat) => {{
         let ret = type_check!($lhs, $rhs, $ctx)?;
