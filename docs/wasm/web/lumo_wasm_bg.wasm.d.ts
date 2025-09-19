2bb179a2e333eb19c3175d0aba96618db28377f1
秋井乃音斗 <archy1none@gmail.com>
2025-09-19 16:09:38 +0900

    Update lumo_wasm_bg.wasm.d.ts

--- docs/wasm/web/lumo_wasm_bg.wasm.d.ts
+++ docs/wasm/web/lumo_wasm_bg.wasm.d.ts
@@ -2,8 +2,8 @@
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
 export const __wbg_lumo_free: (a: number, b: number) => void;
-export const lumo_get_bytecode: (a: number) => [number, number];
-export const lumo_get_return_type: (a: number) => [number, number];
+export const lumo_bytecode: (a: number) => [number, number];
+export const lumo_return_type: (a: number) => [number, number];
 export const lumo: (a: number, b: number) => [number, number, number];
 export const __wbindgen_export_0: WebAssembly.Table;
 export const __wbindgen_free: (a: number, b: number, c: number) => void;
