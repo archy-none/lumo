927d5adee52404c965a70cb3d683fa1459e36f64
秋井乃音斗 <archy1none@gmail.com>
2025-09-19 16:09:31 +0900

    Update node.mjs

--- docs/runtime/node.mjs
+++ docs/runtime/node.mjs
@@ -1,44 +1,20 @@
 import { lumo as compile } from "../wasm/node/lumo_wasm.js";
-import { LumoNodeLib } from "./lib/std.mjs";
-import { LumoMathLib } from "./lib/math.mjs";
-import { LumoOSLib } from "./lib/os.mjs";
-import { LumoRandomLib } from "./lib/random.mjs";
-import { LumoDatetimeLib } from "./lib/datetime.mjs";
-import { LumoTimeLib } from "./lib/time.mjs";
-import { module } from "./module.mjs";
+import { LumoNodeLib } from "./stdlib.mjs";
 import { read } from "./ffi.mjs";
 
-const moduleClasses = {
-    math: LumoMathLib,
-    os: LumoOSLib,
-    random: LumoRandomLib,
-    datetime: LumoDatetimeLib,
-    time: LumoTimeLib,
-};
-
-export async function lumo(code, customModules = {}) {
+export async function lumo(code) {
     const result = compile(code);
-    const returnType = eval(`(${result.get_return_type()})`);
-    const bytecodes = result.get_bytecode().buffer;
-    const moduleObj = await WebAssembly.compile(bytecodes);
-    const importsInfo = WebAssembly.Module.imports(moduleObj);
-    const stdLib = new LumoNodeLib();
-    const importObject = { env: { ...stdLib.bridge() } };
-    const instances = { LumoNodeLib: stdLib };
-    module({
-        importsInfo,
-        moduleClasses,
-        customModules,
-        instances,
-        importObject,
-        runtime: "Node",
-    });
+    const returnType = eval(result.return_type());
+    const bytecodes = result.bytecode().buffer;
 
-    const wab = bytecodes;
-    const { instance } = await WebAssembly.instantiate(wab, importObject);
-    Object.values(instances).forEach((inst) => inst.set_wasm(instance));
+    const stdlib = new LumoNodeLib();
+    const { instance } = await WebAssembly.instantiate(bytecodes, {
+        env: stdlib.bridge(),
+    });
+    stdlib.set_wasm(instance);
+
     const raw = instance.exports._start();
-    if (returnType != null) {
+    if (returnType !== "void") {
         return read(instance, returnType, raw);
     }
 }
