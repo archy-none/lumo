246cb5bb389d4834cf7cf1395477b0085e45125b
秋井乃音斗 <archy1none@gmail.com>
2025-09-19 16:14:56 +0900

    Update web.mjs

--- docs/runtime/web.mjs
+++ docs/runtime/web.mjs
@@ -1,57 +1,20 @@
-import init, { lumo as compile } from "../wasm/web/lumo_wasm.js";
-import { LumoWebLib } from "./lib/std.mjs";
-import { LumoMathLib } from "./lib/math.mjs";
-import { LumoRandomLib } from "./lib/random.mjs";
-import { LumoDatetimeLib } from "./lib/datetime.mjs";
-import { LumoTimeLib } from "./lib/time.mjs";
-import { module } from "./module.mjs";
+import { lumo as compile } from "../wasm/web/lumo_wasm.js";
+import { LumoWebLib } from "./stdlib.mjs";
 import { read } from "./ffi.mjs";
 
-const moduleClasses = {
-    math: LumoMathLib,
-    random: LumoRandomLib,
-    datetime: LumoDatetimeLib,
-    time: LumoTimeLib,
-};
-
-await init();
-export async function lumo(code, customModules = {}) {
+export async function lumo(code) {
     const result = compile(code);
-    const returnType = eval(`(${result.get_return_type()})`);
-    const bytecodes = result.get_bytecode().buffer;
-    const moduleObj = await WebAssembly.compile(bytecodes);
-    const importsInfo = WebAssembly.Module.imports(moduleObj);
-    const stdLib = new LumoWebLib();
-    const importObject = { env: { ...stdLib.bridge() } };
-    const instances = { LumoWebLib: stdLib };
-    module({
-        importsInfo,
-        moduleClasses,
-        customModules,
-        instances,
-        importObject,
-        runtime: "Web",
-    });
+    const returnType = eval(result.return_type());
+    const bytecodes = result.bytecode().buffer;
 
-    const wab = bytecodes;
-    const { instance } = await WebAssembly.instantiate(wab, importObject);
-    Object.values(instances).forEach((inst) => inst.set_wasm(instance));
+    const stdlib = new LumoWebLib();
+    const { instance } = await WebAssembly.instantiate(bytecodes, {
+        env: stdlib.bridge(),
+    });
+    stdlib.set_wasm(instance);
+
     const raw = instance.exports._start();
-    if (returnType != null) {
+    if (returnType !== "void") {
         return read(instance, returnType, raw);
     }
 }
-
-class Lumo extends HTMLElement {
-    constructor() {
-        super();
-        console.log("Welcome to the Lumo programming!");
-    }
-
-    async connectedCallback() {
-        await lumo(this.innerHTML);
-        this.remove();
-    }
-}
-
-customElements.define("lumo-code", Lumo);
