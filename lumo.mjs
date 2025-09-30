9ab275ac442f7f0d70ec677d288508d19a09ce1c
秋井乃音斗 <archy1none@gmail.com>
2025-09-30 20:44:12 +0900

    Update lumo.mjs

--- lumo.mjs
+++ lumo.mjs
@@ -2,9 +2,17 @@ import { readFileSync } from "node:fs";
 import { createInterface } from "readline";
 import { lumo } from "./docs/runtime/node.mjs";
 
-const args = process.argv.slice(2);
-if (args.length === 0) {
-    let code = "";
+async function runScript(source) {
+    try {
+        const result = await lumo(source.toString());
+        if (result !== undefined) console.log(result);
+    } catch (e) {
+        console.log("\u0007Error!", e);
+    }
+}
+
+function repl(lib = "") {
+    let code = lib;
     const rl = createInterface({
         input: process.stdin,
         output: process.stdout,
@@ -14,31 +22,32 @@ if (args.length === 0) {
     console.log("Lumo REPL");
     rl.prompt();
 
-    rl.on("line", (input) => {
-        if (input.trim() !== "")
-            lumo(`${code};${input}`)
-                .then((result) => {
-                    code += `;${input}`;
-                    if (result !== undefined) console.log(result);
-                })
-                .catch((e) => console.log("\u0007Error!", e))
-                .then(() => rl.prompt());
-        else rl.prompt();
+    rl.on("line", async (input) => {
+        if (input.trim() !== "") {
+            try {
+                let result = await lumo(`${code};${input}`);
+                if (result !== undefined) console.log(result);
+                code += `;${input}`;
+            } catch (e) {
+                console.log("\u0007Error!", e);
+            }
+        }
+        rl.prompt();
     });
 
     rl.on("close", () => {
         console.log("Bye");
         process.exit(0);
     });
-} else if (args.length === 2 && args[0] === "-l") {
-    console.log(await lumo(args[1]));
-} else {
-    const filePath = args[0];
-    const source = readFileSync(filePath, "utf8");
-    try {
-        const result = await lumo(source.toString());
-        if (result !== undefined) console.log(result);
-    } catch (e) {
-        console.log("Error!", e);
-    }
+}
+
+const args = process.argv.slice(2);
+if (args.length === 0) {
+    repl();
+} else if (args.length === 2 && (args[0] === "--line" || args[0] === "-l")) {
+    runScript(args[1]);
+} else if (args.length === 2 && (args[0] === "--debug" || args[0] === "-d")) {
+    repl(readFileSync(args[1], "utf8"));
+} else if (args.length === 1) {
+    runScript(readFileSync(args[0], "utf8"));
 }
